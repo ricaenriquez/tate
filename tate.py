0aa781da97295d81bf58bc33a1d15550e8020176
rica <rica@alumni.stanford.edu>
2015-05-17 20:51:22 -0500

    Explored a bit more with presenter groups and changed heatmap to percent of pieces

--- tate.py
+++ tate.py
@@ -66,6 +66,27 @@ df_year_known_small = df_year_known[(df_year_known.acquisition == 'accepted') |
 df_year_known_small = df_year_known_small[df_year_known_small.year <= df_year_known_small.acquisitionYear].reset_index()
 df_year_known_small.acquisitionYear = df_year_known_small.acquisitionYear.astype(np.int64)
 
+def find_presented(entry):
+    if ('presented by the artist' in entry.lower()) | ('and the artist' in entry.lower()) \
+            | ("artist's estate" in entry.lower()) | ('estate of the artist' in entry.lower()):
+        return 'Artist'
+    elif ('society' in entry.lower()) | ('fund' in entry.lower()) | ('foundation' in entry.lower()) | ('loan' in entry.lower()) \
+            | ('subscribers' in entry.lower()) | ('bequest' in entry.lower()) | ('committee' in entry.lower()) \
+            | ('patrons' in entry.lower()) | ('members' in entry.lower()) | ('university' in entry.lower()) \
+            | ('institute' in entry.lower()) | ('museum' in entry.lower()) | ('gallery' in entry.lower()) \
+            | ('galleries' in entry.lower()) | ('council' in entry.lower()) | ('federation' in entry.lower()) \
+            | ('ministry' in entry.lower()) | ('association' in entry.lower()) | ('trust' in entry.lower()) \
+            | ('friends' in entry.lower()) | ('collection' in entry.lower()) | ('galerie' in entry.lower()):
+        return 'Groups'
+    elif ('anonymous' in entry.lower()) | ('private collector' in entry.lower()) | ('by' not in entry.lower()):
+        return 'Anonymous'
+    else:
+        # print entry
+        return 'Named Individuals'
+df_year_known_small['Artist Presented'] = 1
+df_year_known_small['Artist Presented'] = df_year_known_small[df_year_known_small.acquisition == 'presented'].\
+    apply(lambda row: find_presented(row.creditLine), axis=1)
+
 # Plot after 1856 because there were 37,403 Turner paintings accepted that year, which shrinks the bar plot for accepted
 # Plot number pieces per year for each method
 years = np.arange(1875,2025)
@@ -76,9 +97,33 @@ g.set_xlabels(label='Acquisition Year')
 g.set_xticklabels(step=25)
 sns.plt.show()
 
+presented_1975_artists = df_year_known[(df_year_known.acquisition == 'presented') & (df_year_known.acquisitionYear == 1975)]['artist'].unique()
+print 'Number of unique artists purchased in 1997 was', len(presented_1975_artists)
+pieces_1975 = len(df_year_known[(df_year_known['acquisitionYear'] == 1975) & (df_year_known["acquisition"] == 'presented')])
+max_pieces = 0
+print 'Artists whose pieces were presented in 1975 that had the most pieces, # pieces, and percent of total pieces presented that year'
+for artist in presented_1975_artists:
+    pieces = len(df_year_known[(df_year_known['artist'] == artist) &
+                     (df_year_known['acquisitionYear'] == 1975) & (df_year_known["acquisition"] == 'presented')])
+    if pieces > max_pieces:
+        max_pieces = pieces
+        print artist, max_pieces, float(max_pieces)/float(pieces_1975)*100.
+
+purchased_1997_artists = df_year_known[(df_year_known.acquisition == 'purchased') & (df_year_known.acquisitionYear == 1997)]['artist'].unique()
+print 'Number of unique artists purchased in 1997 was', len(purchased_1997_artists)
+pieces_1997 = len(df_year_known[(df_year_known['acquisitionYear'] == 1997) & (df_year_known["acquisition"] == 'purchased')])
+max_pieces = 0
+print 'Artists whose pieces were purchased in 1997 that had the most pieces, # pieces, and percent of total pieces presented that year'
+for artist in purchased_1997_artists:
+    pieces = len(df_year_known[(df_year_known['artist'] == artist) &
+                     (df_year_known['acquisitionYear'] == 1997) & (df_year_known["acquisition"] == 'purchased')])
+    if pieces > max_pieces:
+        max_pieces = pieces
+        print artist, max_pieces, float(max_pieces)/float(pieces_1997)*100.
+
 # Plot relation of acquisition year and year art piece completed
 h = sns.FacetGrid(df_year_known_small, col='acquisition', hue='acquisition', margin_titles=True,
-                  xlim=(1823, 2013), ylim=(1545, 2013), palette=sns.color_palette("hls", 4))
+                  xlim=(1823, 2013), ylim=(1545, 2013), palette=sns.color_palette("hls", 4), legend_out=True)
 h.map(plt.scatter, 'acquisitionYear', 'year', s=25, alpha=.25)
 for ax in h.axes.flat:
     ax.plot((1823, 2013), (1823, 2013), c=".2", ls="--")
@@ -86,6 +131,53 @@ h.set_xlabels(label='Acquisition Year')
 h.set_ylabels(label="Year of 'Completion'")
 h.set_titles(['Accepted', 'Bequeathed', 'Presented', 'Purchased'])
 sns.plt.show()
+
+# Plot types of donors pieces were presented
+df_year_known_small_presented = df_year_known_small[df_year_known_small['acquisition'] == 'presented']
+# h = sns.lmplot('acquisitionYear', 'year', df_year_known_small_presented, 'Artist Presented', fit_reg=False, s=25, alpha=.25)
+h = sns.FacetGrid(df_year_known_small_presented, col='Artist Presented', hue='Artist Presented', margin_titles=True,
+                  xlim=(1823, 2013), ylim=(1545, 2013), palette=sns.color_palette("hls", 4), legend_out=False)
+h.map(plt.scatter, 'acquisitionYear', 'year', s=25, alpha=.25)
+for ax in h.axes.flat:
+    ax.plot((1823, 2013), (1823, 2013), c=".2", ls="--")
+h.set_xlabels(label='Acquisition Year')
+h.set_ylabels(label="Year of 'Completion'")
+h.set_titles('Presented Pieces by Presenter Type')
+sns.plt.show()
+
+# If year of acquisition is the same year of completion plot donor type
+df_year_known_small_presented_same = df_year_known_small_presented[df_year_known_small_presented['acquisitionYear']
+                                                                   == df_year_known_small_presented['year']]
+
+h = sns.barplot(x='Artist Presented', data=df_year_known_small_presented_same, ci=None, palette=sns.color_palette("hls", 4), hline=.1)
+# h.set_xlabels(label='Presenter Type')
+# h.set_ylabels(label="Number of Pieces")
+# h.set_titles('Presented Pieces by Presenter Type')
+sns.plt.show()
+
+
+df_groups = df_year_known_small_presented_same[df_year_known_small_presented_same['Artist Presented'] == 'Groups']
+groups = df_groups.creditLine.unique()
+
+def find_group(entry):
+    if 'Chantrey Bequest' in entry:
+        return 'Chantrey Bequest'
+    elif 'Contemporary Art Society' in entry:
+        return 'Contemporary Art Society'
+    elif 'Institute of Contemporary Prints' in entry:
+        return 'Institute of Contemporary Prints'
+    elif 'Tate' in entry:
+        return 'Tate Patrons/Friends'
+
+df_groups['Groups'] = df_groups.apply(lambda row: find_group(row.creditLine), axis=1)
+h=sns.barplot(x='Groups', data=df_groups, ci=None, palette=sns.color_palette("hls", 4), hline=.1)
+# h.set_xlabels(label='Presenter Group Type')
+# h.set_ylabels(label="Number of Pieces")
+sns.plt.show()
+
+
+
+
 
 # See which countries/continents artists came from for each year
 def replace_country(entry):
@@ -296,13 +388,18 @@ df_post_1950 = df_year_known_small[(df_year_known_small.year >= 1950)]
 df_post_1950['Artist Birth Continent'] = df_post_1950.apply(lambda x: (get_continent(x.artistId)), axis=1)
 
 # Let's focus on pieces not in Europe!
-df_post_1950_cut = df_post_1950[(df_post_1950['Artist Birth Continent'] != 'Europe')]
+df_post_1950_cut = df_post_1950[(df_post_1950['Artist Birth Continent'] != 'Europe')].reset_index()
 df_post_1950_cut["Year Artwork 'Completed'"] = df_post_1950_cut['year']
 
-grouped = DataFrame({'pieces': df_post_1950_cut.groupby(["Year Artwork 'Completed'", 'Artist Birth Continent']).size()}).reset_index()
+
+
+print (df_post_1950_cut.groupby(["Year Artwork 'Completed'"]).size().sum())
+
+grouped = DataFrame({'pieces': df_post_1950_cut.groupby(["Year Artwork 'Completed'", 'Artist Birth Continent']).size()
+                               /(df_post_1950_cut.groupby(["Year Artwork 'Completed'"]).size())*100.}).reset_index()
 grouped_rect = grouped.pivot('Artist Birth Continent', "Year Artwork 'Completed'", 'pieces')
 grouped_rect = grouped_rect.fillna(0)
-sns.heatmap(grouped_rect, yticklabels=['Unknown', 'Africa', 'Asia', 'North America', 'Oceania', 'South America'])
+h = sns.heatmap(grouped_rect, yticklabels=['Unknown', 'Africa', 'Asia', 'North America', 'Oceania', 'South America'])
 plt.show()
 
 # Look to see which artists were hot in Asia and South America
@@ -326,3 +423,24 @@ for artist in Asian_artists:
                      (df_post_1950_cut["Year Artwork 'Completed'"] == 2007)]),\
         df_post_1950_cut[(df_post_1950_cut['artist'] == artist) &
                          (df_post_1950_cut["Year Artwork 'Completed'"] == 2007)]['acquisitionYear'].unique()
+
+Asian_artists = df_post_1950_cut[(df_post_1950_cut['Artist Birth Continent'] == 'Asia') &
+                                 (df_post_1950_cut["Year Artwork 'Completed'"] == 2011)].artist.unique()
+
+print "Asian Artists with work completed in 2011, number of pieces, and [acquisition year]"
+for artist in Asian_artists:
+        print artist, len(df_post_1950_cut[(df_post_1950_cut['artist'] == artist) &
+                     (df_post_1950_cut["Year Artwork 'Completed'"] == 2011)]),\
+        df_post_1950_cut[(df_post_1950_cut['artist'] == artist) &
+                         (df_post_1950_cut["Year Artwork 'Completed'"] == 2011)]['acquisitionYear'].unique()
+
+
+Asian_artists = df_post_1950_cut[(df_post_1950_cut['Artist Birth Continent'] == 'Asia') &
+                                 (df_post_1950_cut["Year Artwork 'Completed'"] == 2012)].artist.unique()
+
+print "Asian Artists with work completed in 2012, number of pieces, and [acquisition year]"
+for artist in Asian_artists:
+        print artist, len(df_post_1950_cut[(df_post_1950_cut['artist'] == artist) &
+                     (df_post_1950_cut["Year Artwork 'Completed'"] == 2012)]),\
+        df_post_1950_cut[(df_post_1950_cut['artist'] == artist) &
+                         (df_post_1950_cut["Year Artwork 'Completed'"] == 2012)]['acquisitionYear'].unique()
